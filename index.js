adf42e3f62e9dd875f9d6282264dcbacf7ec4f26
Igor Yevtushenko <70605781+IgorYevtushenkoUA@users.noreply.github.com>
2020-10-18 19:30:22 +0300

    feat: get random us text

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import {data_text_ua} from "./data/ukrainian_texts.js";
+import {LL1} from "./LL1.js";
+
+
 document.getElementById("btn-instruction").addEventListener("click", function (e) {
     let modal = document.getElementById("modal-window-instruction")
     modal.style.display = "block"
@@ -15,7 +19,19 @@ document.getElementById("btn-save").addEventListener("click", function (event) {
     alert(surname)
 })
 
-document.getElementById("btn-random-text").addEventListener("click",function (event) {
-    // do interesting things
+document.getElementById("btn-random-text").addEventListener("click", function (event) {
+    let randomText = Math.floor(Math.random() * data_text_ua.size)
+    document.getElementById("textarea").value = data_text_ua.get(randomText)
+})
+
+document.getElementById("btn-start").addEventListener("click", function (event) {
+    let text = document.getElementById("textarea").value
+    let surname = document.getElementById("input-surname-field").value
+    if (text === "") {
+        alert("Введіть текст або натисніть на рандомний текст")
+    } else {
+        let ll1 = new LL1(text, surname)
+
+    }
 })
 
