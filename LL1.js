0a3be1cb7fc5c3116597a9c7a5d6b15929d94242
Igor Yevtushenko <70605781+IgorYevtushenkoUA@users.noreply.github.com>
2020-10-26 13:56:48 +0200

    fix: fixed bags in regex

--- LL1.js
+++ LL1.js
@@ -1,33 +1,56 @@
 export class LL1 {
     'use strict'
+
     constructor(text = "", surname = "Євтушенко") {
         this._text = text
         this._surname = surname
     }
 
-    setText(text){this._text = text}
-    setSurname(surname){this._surname = surname}
+    setText(text) {
+        this._text = text
+    }
 
-    _surnameWords  = []
-    _noSurnameWords  = []
-    _numberWords  = []
+    setSurname(surname) {
+        this._surname = surname
+    }
+
+    _surnameWords = []
+    _noSurnameWords = []
+    _numberWords = []
     _emailWords = []
-    _phoneNumberWords  = []
+    _phoneNumberWords = []
     _notUnderstandWords = []
 
-    get getSurnameWords(){return this._surnameWords }
-    get getNoSurnameWords(){return this._noSurnameWords }
-    get getNumberWords(){return this._numberWords }
-    get getEmailWords(){return this._emailWords}
-    get getPhoneNumberWords(){return this._phoneNumberWords }
-    get getNotUnderstandWords(){return this._notUnderstandWords}
+    get getSurnameWords() {
+        return this._surnameWords
+    }
 
-    clearDaraArr(){
-        this._surnameWords  = []
-        this._noSurnameWords  = []
-        this._numberWords  = []
+    get getNoSurnameWords() {
+        return this._noSurnameWords
+    }
+
+    get getNumberWords() {
+        return this._numberWords
+    }
+
+    get getEmailWords() {
+        return this._emailWords
+    }
+
+    get getPhoneNumberWords() {
+        return this._phoneNumberWords
+    }
+
+    get getNotUnderstandWords() {
+        return this._notUnderstandWords
+    }
+
+    clearDaraArr() {
+        this._surnameWords = []
+        this._noSurnameWords = []
+        this._numberWords = []
         this._emailWords = []
-        this._phoneNumberWords  = []
+        this._phoneNumberWords = []
         this._notUnderstandWords = []
     }
 
@@ -36,7 +59,7 @@ export class LL1 {
     }
 
     isDecimal(ch) {
-        return ch.match("/[0-9]/")
+        return ch.match(/[0-9]/i)
     }
 
     /**
@@ -44,10 +67,23 @@ export class LL1 {
      * @param {string} word
      * @returns {*}
      */
-    delLasCharIfNotLetterOrDecimal(word) {
-        while (!this.isLetter(word.charAt(word.length - 1)) && this.isDecimal(word.charAt(word.length - 1))) {
+    delLastCharIfNotLetterOrDecimal(word) {
+        while (!this.isLetter(word.charAt(word.length - 1)) && !this.isDecimal(word.charAt(word.length - 1)) && word.length != 0) {
             word = word.slice(0, word.length - 1)
         }
+        return word
+    }
+
+    delFirstCharIfNotLetterOrDecimal(word) {
+        while (!this.isLetter(word.charAt(0)) && !this.isDecimal(word.charAt(0)) && word.length != 0) {
+            word = word.slice(1, word.length)
+        }
+        return word
+    }
+
+    delIncorrectSymbolsFromStartAndEnd(word) {
+        word = this.delLastCharIfNotLetterOrDecimal(word)
+        word = this.delFirstCharIfNotLetterOrDecimal(word)
         return word
     }
 
@@ -80,7 +116,7 @@ export class LL1 {
      * @returns {boolean}
      */
     isNumberRegex(word) {
-        let reg = /^[-]?[\d]+([.]?[\d]+)?$/im
+        let reg = /^[-]?[\d]+([.,]?[\d]+)?$/im
         let result = word.match(reg)
         return result !== null
     }
@@ -101,8 +137,14 @@ export class LL1 {
      * @param {string} word
      * @returns {boolean}
      */
-    isPhoneRegex(word) {
+    isFullPhoneRegex(word) {
         let reg = /^[\+]?(38)?[ ]?[(]?[\d ]{3}[)]?[ ]?[\d]{3}[-]?[ ]?[\d]{2}[-]?[ ]?[\d]{2}$/im
+        let result = word.match(reg)
+        return result !== null
+    }
+
+    isShortPhoneRegex(word) {
+        let reg = /^[ ]?[\d]{3}[-]?[ ]?[\d]{2}[-]?[ ]?[\d]{2}$/im
         let result = word.match(reg)
         return result !== null
     }
@@ -113,21 +155,24 @@ export class LL1 {
         console.log(this._surname)
         console.log(text)
         for (let i = 0; i < text.length; i++) {
-            text[i] = this.delLasCharIfNotLetterOrDecimal(text[i])
-            let word = text[i]
+            let word = this.delIncorrectSymbolsFromStartAndEnd(text[i])
+
+            console.log(word)
+            debugger
             if (this.isSurnameWordRegex(word, this._surname)) {
                 this._surnameWords.push(word)
             } else if (this.isNotSurnameWordRegex(word)) {
-                this._noSurnameWords .push(word)
+                this._noSurnameWords.push(word)
             } else if (this.isNumberRegex(word)) {
-                this._numberWords .push(word)
+                this._numberWords.push(word)
             } else if (this.isEmailRegex(word)) {
                 this._emailWords.push(word)
-            } else if (this.isPhoneRegex(word)) {
-                this._phoneNumberWords .push(word)
+            } else if (this.isFullPhoneRegex(word) || this.isShortPhoneRegex(word)) {
+                this._phoneNumberWords.push(word)
             } else {
                 this._notUnderstandWords.push(word)
             }
         }
     }
 }
+
